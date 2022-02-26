e4d1f4b76413a06f5a21c9c9f7d09652f6ef2071
minwu3 <77194824+minwu3@users.noreply.github.com>
2022-02-26 14:31:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #Password Generator Project
+# import package
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+# set inupt variables
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
@@ -13,19 +15,27 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 # Eazy Level - Order not randomised:
 # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
 easy_code = ''
+
+# randomly get letter
 for j in range(0,nr_letters):  
   random_num = random.randint(0,len(letters)-1)
   for i in letters:
     if i == letters[random_num]:
       easy_code += i
-      
+
+
+# randomly get number
 for j in range(0,nr_numbers):  
   random_num = random.randint(0,len(numbers)-1)
   for i in numbers:
     if i == numbers[random_num]:
       easy_code += i
 
+      
+# randomly get symbol      
 for j in range(0,nr_symbols):  
   random_num = random.randint(0,len(symbols)-1)
   for i in symbols:
@@ -47,9 +57,9 @@ for j in range(0,nr_symbols):
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-hard_code = []
 
-total_code_num = nr_letters + nr_numbers + nr_symbols
+# create a list to contain the string we obtain
+hard_code = []
 
 for j in range(0,nr_letters):  
   random_num = random.randint(0,len(letters)-1)
@@ -69,7 +79,7 @@ for j in range(0,nr_symbols):
     if i == symbols[random_num]:
       hard_code += i
       
-
+# rearrange the list
 random.shuffle(hard_code)
 
 hard_fin_code = ''
